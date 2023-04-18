c76422750ddd2cbed4a0ba2239bf1b3b7a3a93d2
Muxing Chen <ike.chan@outlook.com>
2023-04-17 20:04:21 -0400

    disable hover on mobile

--- gsap.js
+++ gsap.js
@@ -100,45 +100,58 @@ tl.from(
   },
   '<'
 )
+
+const isMobile = function () {
+  return (
+    navigator.maxTouchPoints > 0 && /Android|iPhone/i.test(navigator.userAgent)
+  )
+}
+console.log(isMobile())
+
 // hovering effect
-const projects = document.querySelectorAll('.project')
+if (!isMobile()) {
+  const projects = document.querySelectorAll('.project')
 
-projects.forEach((item) => {
-  const imageWrapper = item.querySelector('.image-wrapper')
-  const imageWrapperBounds = imageWrapper.getBoundingClientRect()
-  let itemBounds = item.getBoundingClientRect()
+  projects.forEach((item) => {
+    const imageWrapper = item.querySelector('.image-wrapper')
+    const imageWrapperBounds = imageWrapper.getBoundingClientRect()
+    let itemBounds = item.getBoundingClientRect()
 
-  const onMouseEnter = () => {
-    gsap.set(imageWrapper, {
-      scale: 0.8,
-      // xPercent: 25,
-      // yPercent: 50,
-      // rotation: -15,
-    })
-    gsap.to(imageWrapper, { opacity: 1, scale: 1, yPercent: 0, rotation: 0 })
-  }
+    const onMouseEnter = () => {
+      gsap.set(imageWrapper, {
+        scale: 0.8,
+        // xPercent: 25,
+        // yPercent: 50,
+        // rotation: -15,
+      })
+      gsap.to(imageWrapper, { opacity: 1, scale: 1, yPercent: 0, rotation: 0 })
+    }
 
-  const onMouseLeave = () => {
-    gsap.to(imageWrapper, {
-      opacity: 0,
-      // yPercent: -50,
-      // xPercent: 25,
-      scale: 0.8,
-      // rotation: -15,
-    })
-  }
+    const onMouseLeave = () => {
+      gsap.to(imageWrapper, {
+        opacity: 0,
+        // yPercent: -50,
+        // xPercent: 25,
+        scale: 0.8,
+        // rotation: -15,
+      })
+    }
 
-  const onMouseMove = ({ x, y }) => {
-    let yOffset = itemBounds.top / imageWrapperBounds.height
-    yOffset = gsap.utils.mapRange(0, 1.5, -150, 150, yOffset)
-    gsap.to(imageWrapper, {
-      duration: 1.25,
-      x: Math.abs(x - itemBounds.left) - imageWrapperBounds.width / 1.55,
-      y: Math.abs(y - itemBounds.top) - imageWrapperBounds.height / 2 - yOffset,
-    })
-  }
+    const onMouseMove = ({ x, y }) => {
+      let yOffset = itemBounds.top / imageWrapperBounds.height
+      yOffset = gsap.utils.mapRange(0, 1.5, -150, 150, yOffset)
+      gsap.to(imageWrapper, {
+        duration: 1.25,
+        x: Math.abs(x - itemBounds.left) - imageWrapperBounds.width / 1.55,
+        y:
+          Math.abs(y - itemBounds.top) -
+          imageWrapperBounds.height / 2 -
+          yOffset,
+      })
+    }
 
-  item.addEventListener('mouseenter', onMouseEnter)
-  item.addEventListener('mouseleave', onMouseLeave)
-  item.addEventListener('mousemove', onMouseMove)
-})
+    item.addEventListener('mouseenter', onMouseEnter)
+    item.addEventListener('mouseleave', onMouseLeave)
+    item.addEventListener('mousemove', onMouseMove)
+  })
+}
